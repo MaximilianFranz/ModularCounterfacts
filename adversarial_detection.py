418f86f985a0671360cf2bd858bbd6e07d90de25
Maximilian Franz <framax@iosb.fraunhofer.de>
2018-11-15 17:21:32 +0100

    make class and use instance variables

--- adversarial_detection.py
+++ adversarial_detection.py
@@ -16,21 +16,18 @@ style.use("ggplot")
 
 class AdversarialDetection():
 
-    def __init__(self, dataset_file, clf):
+    def __init__(self, clf, chosen_attributes=None):
         """
-
-        Args:
-            dataset_file:
-            clf:
         """
         self.X, self.Y = self.load_data_txt(normalize=True)
         self.clf = clf
         X_train, X_test, Y_train, Y_test = train_test_split(self.X, self.Y, test_size=0.2, random_state=1000)
         self.clf.fit(X_train, Y_train)
+        self.chosen_attributes = chosen_attributes
 
 
 
-    def load_data_txt(normalize=False):
+    def load_data_txt(self, normalize=False):
         data = pd.read_csv("UCI_Credit_Card.csv")
 
         Y = np.array(data.pop("default.payment.next.month"))
@@ -42,133 +39,142 @@ class AdversarialDetection():
         return X, Y
 
 
-def get_border_touchpoints(support_points, original_instance, predictor_fn, fineness=5):
-    """
-    Uses a sort of binary search to find the border touchpoint on segments faster.
-    """
-    touchpoints = []
-    for point in support_points:
-        # search on segment between point and original_instance with binary search
-        l = 0
-        r = 1
-        for i in range(fineness):
-            m = (r + l)/ 2.0
-            x_m = original_instance + m*(point - original_instance)
-            if predictor_fn.predict_proba(np.array(x_m).reshape(1,-1))[0,1] <= 0.5:
-                l = m
-            else:
-                r = m
-        touchpoints.append(x_m)
+    def get_border_touchpoints(self, support_points, original_instance, predictor_fn, fineness=5):
+        """
+        Uses a sort of binary search to find the border touchpoint on segments faster.
+        """
+        touchpoints = []
+        for point in support_points:
+            # search on segment between point and original_instance with binary search
+            l = 0
+            r = 1
+            for i in range(fineness):
+                m = (r + l)/ 2.0
+                x_m = original_instance + m*(point - original_instance)
+                if predictor_fn.predict_proba(np.array(x_m).reshape(1,-1))[0,1] <= 0.5:
+                    l = m
+                else:
+                    r = m
+            touchpoints.append(x_m)
 
-    return np.array(touchpoints)
-
-
-def sample_around(border_touchpoints, num_samples, sigma):
-    """
-    Samples around the border_touchpoints with a normal distribution to generate
-    a dataset for training a linear model which yields the explanation
-
-    Normal distribution is parametrized based on the distribution of
-    the border_touchpoints, so that we sample along the decision boundary
-
-    """
-    max_arg = np.amax(border_touchpoints, axis=0)
-    min_arg = np.amin(border_touchpoints, axis=0)
-    result = np.array(border_touchpoints)
-    num_per_point = int(num_samples / len(border_touchpoints))
-    sigmas = (max_arg - min_arg)*sigma
-    for point in border_touchpoints:
-        mean = point
-        cov = np.diag(sigmas**2)
-        rand = np.random.multivariate_normal(mean, cov, num_per_point)
-        result = np.append(result, rand, axis=0)
-
-    return result
+        self.boundary_touchpoints = np.array(touchpoints)
+        return self.boundary_touchpoints
 
 
-def get_first_adversarial(original_instance, predictor, dataset, chosen_attributes):
-    """
-    Using the GradientSearch approach this method searches the first adversarial
-    instance to feed into magnetic_sampling.
-    """
-    # GradientSearch returns only the two chosen attributes
-    dec = gradientgrow.decision(dataset, chosen_attributes, original_instance, predictor)
-    dec.gradientSearch(step=0.05, scale=1.0, nsample=100)
-    return dec
-    # return dec.get_last_instance()
+    def sample_around(self, border_touchpoints, num_samples, sigma):
+        """
+        Samples around the border_touchpoints with a normal distribution to generate
+        a dataset for training a linear model which yields the explanation
 
-def explain_instance(instance, predictor_fn, dataset, chosen_attributes):
-    """
-    Using the functions provided in this module this returns a linear
-    """
-    dec = get_first_adversarial(instance, predictor_fn, dataset, chosen_attributes)
-    first_adversarial = dec.get_last_instance()
-    path = np.array(dec.GSP)
+        Normal distribution is parametrized based on the distribution of
+        the border_touchpoints, so that we sample along the decision boundary
 
-    # magnetic_sampling uses the predictor_fn not the predictor, thus pass the corresponding fct
-    support_points = magnetic_sampling(predictor_fn.predict_proba,
-                            instance,
-                            first_adversarial,
-                            num_samples=15,
-                            features=chosen_attributes,
-                            sector_width=0.35,
-                            confidence=5,
-                            threshold=2
-                            )
+        """
+        max_arg = np.amax(border_touchpoints, axis=0)
+        min_arg = np.amin(border_touchpoints, axis=0)
+        result = np.array(border_touchpoints)
+        num_per_point = int(num_samples / len(border_touchpoints))
+        sigmas = (max_arg - min_arg)*sigma
+        for point in border_touchpoints:
+            mean = point
+            cov = np.diag(sigmas**2)
+            rand = np.random.multivariate_normal(mean, cov, num_per_point)
+            result = np.append(result, rand, axis=0)
 
-    border_touchpoints = get_border_touchpoints(support_points,
-                                                instance,
-                                                predictor_fn,
-                                                fineness=5)
-    X = sample_around(border_touchpoints, 1000, 0.1)
+        self.sample_set = result
+        return self.sample_set
 
-    y = predictor_fn.predict_proba(X)[:,1]
-    pos = y[y > 0.5]
-    neg = y[y <= 0.5]
-    print('positive ', pos.size)
-    print('negative ', neg.size)
 
-    # generate colormap
-    colors = []
-    for y_i in y:
-        if y_i > 0.5:
-            colors.append('limegreen')
-        else:
-            colors.append('tomato')
+    def get_first_adversarial(self, original_instance):
+        """
+        Using the GradientSearch approach this method searches the first adversarial
+        instance to feed into magnetic_sampling.
+        """
+        # GradientSearch returns only the two chosen attributes
+        dec = gradientgrow.decision(self.X,
+                                    self.chosen_attributes,
+                                    original_instance,
+                                    self.clf)
+        dec.gradientSearch(step=0.05, scale=1.0, nsample=100)
+        self.gradientGrow = dec
+        self.first_adversarial = dec.get_last_instance()
+        return self.first_adversarial
 
-    # Plots
-    plt.scatter(X[:, 0], X[:, 5], c=colors, cmap = plt.cm.Paired, marker='.', s=25)
-    plt.scatter([instance[0]], [instance[5]], s=100, c='blue', marker='X')
-    plt.scatter([first_adversarial[0]], [first_adversarial[5]], s=100, c='red', marker='X')
-    plt.scatter([support_points[:,0]], [support_points[:,5]], s=100, c='purple', marker='o')
-    plt.scatter([border_touchpoints[:,0]], [border_touchpoints[:,5]], s=100, c='black', marker='.')
-    # plt.scatter(path[:,1], path[:,2], s=100, c='yellow', marker='.')
-    plt.xlabel("Attribut " + str(0))
-    plt.ylabel("Attribut " + str(5))
-    plt.title("Preliminary Results of adversarial Detection")
-    plt.show()
+    def explain_instance(self, instance, chosen_attributes=None):
+        """
+        Using the functions provided in this module this returns a linear
+        """
+        self.instance = instance
+        if chosen_attributes is not None:
+            self.chosen_attributes = chosen_attributes
+        # TODO: Make sure to handle cases without chosen_attributes accordingly
+        first_adversarial = self.get_first_adversarial(instance);
 
-    dec.sectorSearch(fineness=50)
+        # magnetic_sampling uses the predictor_fn not the predictor,
+        # thus pass the corresponding fct
+        self.support_points = magnetic_sampling(self.clf.predict_proba,
+                                instance,
+                                first_adversarial,
+                                num_samples=15,
+                                features=self.chosen_attributes,
+                                sector_width=0.35,
+                                confidence=5,
+                                threshold=2
+                                )
+
+        self.border_touchpoints = self.get_border_touchpoints(self.support_points,
+                                                    self.instance,
+                                                    self.clf,
+                                                    fineness=5)
+        self.sample_set = self.sample_around(self.border_touchpoints, 1000, 0.1)
+
+        self.predictions = self.clf.predict_proba(self.sample_set)[:,1]
+        self.plot_results()
+        dec.sectorSearch(fineness=50)
+
+    def plot_results(self):
+        if self.predictions is not None:
+
+            pos = self.predictions[self.predictions > 0.5]
+            neg = self.predictions[self.predictions <= 0.5]
+            print('positive ', pos.size)
+            print('negative ', neg.size)
+
+            # generate colormap
+            colors = []
+            for y_i in self.predictions:
+                if y_i > 0.5:
+                    colors.append('limegreen')
+                else:
+                    colors.append('tomato')
+
+
+            # For brevity
+            attr1 = self.chosen_attributes[0]
+            attr2 = self.chosen_attributes[1]
+
+            # Plots
+            plt.scatter(self.sample_set[:, attr1], self.sample_set[:, attr2], c=colors, cmap = plt.cm.Paired, marker='.', s=25)
+            plt.scatter([self.instance[attr1]], [self.instance[attr2]], s=100, c='blue', marker='X')
+            plt.scatter([self.first_adversarial[attr1]], [self.first_adversarial[attr2]], s=100, c='red', marker='X')
+            plt.scatter([self.support_points[:, attr1]], [self.support_points[:, attr2]], s=30, c='purple', marker='o')
+            plt.scatter([self.border_touchpoints[:, attr1]], [self.border_touchpoints[:, attr2]], s=100, c='black', marker='.')
+            plt.xlabel("Attribut " + str(attr1))
+            plt.ylabel("Attribut " + str(attr2))
+            plt.title("Preliminary Results of adversarial Detection")
+            plt.show()
 
 
 def test():
 
+    chosen_attributes = [0,5]
+    clf = RandomForestClassifier(n_jobs=100, n_estimators=50, random_state=5000)
+    explainer = AdversarialDetection(clf=clf, chosen_attributes=chosen_attributes)
+
     X, Y = init.load_data_txt(normalize=True)
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=1000)
 
-    # --- Train Random Forest
-    predictor = RandomForestClassifier(n_jobs=100, n_estimators=50, random_state=5000)
-    predictor.fit(X_train, Y_train)
-
-
-    # --- Accuracy
-    print("Accuracy:", accuracy_score(Y_test, predictor.predict(X_test)))
-    print("Report:\n", classification_report(Y_test, predictor.predict(X_test)))
-
-    # chose two parameters to adjust
-    chosen_attributes = [0,5]
-    print('test instance: ', X_test[15])
-    explain_instance(X_test[15], predictor, X, chosen_attributes)
+    explainer.explain_instance(X_test[15])
 
 if __name__ == '__main__':
     test()
