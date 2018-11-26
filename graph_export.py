3a56bbb86cae70bc76ad4af16a8699fa8b8e6555
Max <franzmaximilian1996@gmail.com>
2018-11-26 17:42:21 +0100

    add utilities to export graphs from scikit.tree
    Also add pydotplus to allow drawing pdf graphs from dot_data

---
+++ graph_export.py
@@ -0,0 +1,32 @@
+from sklearn.tree import export_graphviz
+import pydotplus
+
+
+def export_tree(tree, file_name):
+    if tree is not None:
+        dot_data = export_graphviz(tree, out_file=None, filled=True, rounded=True)
+        graph = pydotplus.graph_from_dot_data(dot_data)
+        graph = make_graph_minimal(graph)
+
+        graph.write_pdf(file_name)
+
+
+def make_graph_minimal(graph):
+    nodes = graph.get_nodes()
+    for node in nodes:
+        old_label = node.get_label()
+        label = prune_label(old_label)
+        if label is not None:
+            node.set_label(label)
+    return graph
+
+
+def prune_label(label):
+    if label is None:
+        return None
+    if len(label) == 0:
+        return None
+    label = label[1:-1]
+    parts = [part for part in label.split('\\n')
+             if 'gini =' not in part and 'samples =' not in part]
+    return '"' + '\\n'.join(parts) + '"'
