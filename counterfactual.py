204cceb78973005a5cf9e337b680c2a396872bb7
Max <franzmaximilian1996@gmail.com>
2019-03-13 16:03:55 +0100

    refine distance metrics

--- counterfactual.py
+++ counterfactual.py
@@ -1,5 +1,6 @@
 from nelder_mead import nelder_mead
 import numpy as np
+from scipy.optimize import minimize
 from statsmodels import robust
 import gradientgrow as gg
 
@@ -10,7 +11,7 @@ class CounterFactualFinder():
         self.data = data
         self.chosen_attributes = chosen_attributes
 
-    def first_counterfactual_with_nelder_mead(self, instance, target_value=1):
+    def first_counterfactual_with_nelder_mead(self, instance, target_value=1, step=10):
         """
 
         Args:
@@ -27,34 +28,38 @@ class CounterFactualFinder():
             X must be np.array
             """
 
-            return target_value - self.clf.predict_proba(x.reshape(1, -1))[0, 1]
+            value = (2*target_value - self.clf.predict_proba(x.reshape(1, -1))[0, 1])**3
+            return value
 
-        return nelder_mead(func, instance)[0]
+        return nelder_mead(func, instance, step=step)[0]
 
-    def improved_nelder_mead(self, instance, target_value=1, weigths=None):
+    def improved_nelder_mead(self, instance, target_value=1, weigths=None, step=10):
 
-        mad = robust.mad(self.data, axis=0)
+        mad = np.array(robust.mad(self.data, axis=0))
+        non_zero = mad[mad != 0] # make sure not to devide by zero
+
+
+        print('mad global', mad)
 
         def manhattan_distance(y, x=instance, weigths=weigths):
 
             if weigths is None:
-                weigths = np.full(len(x), 1)
+                weigths = np.full(len(non_zero), 1)
 
-            abs = np.abs(x - y)
+            abs = np.abs(x - y)[mad != 0]
 
-            result = np.nansum(np.divide(abs, mad) * weigths)
+            result = np.nansum(np.divide(abs, non_zero) * weigths)
             if np.isinf(result):
                 return 0
             return result
 
         def func(x, l=10):
 
-            feat = l*(target_value - self.clf.predict_proba(x.reshape(1,-1))[0, 1])
-            optimize = feat + manhattan_distance(x)
-
+            value = (2*target_value - self.clf.predict_proba(x.reshape(1, -1))[0, 1])**3
+            optimize = l*value + manhattan_distance(x)
             return optimize
 
-        return nelder_mead(func, instance)[0]
+        return nelder_mead(func, instance, step=step)[0]
 
     def get_first_adversarial(self, original_instance):
         """
